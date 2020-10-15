d4f5acfc95b02df91e1b8edfbe7b1c462096eb52
Paul <paul.bible@gmail.com>
2020-10-14 23:32:05 -0400

    Finished single work cluster summary. Works with a simple frequency and count filter.

--- src/sentence_cluster.py
+++ src/sentence_cluster.py
@@ -3,7 +3,8 @@
 """
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
-from scipy.spatial.distance import pdist
+from scipy.spatial.distance import pdist, squareform
+from sklearn import manifold
 import numpy as np
 import nltk
 from collections import defaultdict
@@ -17,7 +18,12 @@ def main():
     print('embeddings loaded')
 
     # load text data from file.
+    # pres = "bush"
+    # year = 2002
+    # filename = '../speeches/%s_%s.txt' % (pres, year)
+    # filename = '../speeches/' + pres + '_' + year + '.txt'
     filename = '../data/lincoln_2nd_SOTU.txt'
+    sentence_files = []
     with open(filename) as f:
         data = f.read()
 
@@ -30,6 +36,7 @@ def main():
     for sentence in sentences:
         # print(sentence)
         sent_vec = et.reduce_sum_word_list(sentence, word_map, matrix)
+        sentence_files.append(filename)
         # print(sent_vec)
         sent_vectors.append(sent_vec)
         # input()
@@ -42,6 +49,7 @@ def main():
     # https://joernhees.de/blog/2015/08/26/scipy-hierarchical-clustering-and-dendrogram-tutorial/
     Y = pdist(sent_mat, 'cosine') # define distance between points (sentence vectors)
     Z = linkage(Y, 'ward') # define linkage, how to group points together
+    print(Y.shape)
 
     # display the resulting clustering of al sentences
     dendrogram(Z, leaf_rotation=90, leaf_font_size=8)
@@ -52,11 +60,14 @@ def main():
     c_labels = fcluster(Z, k, criterion='maxclust')
     print(c_labels)
 
+    output = open('.csv','w')
     # collect the sentence id for each cluster
     cluster_members = defaultdict(list)
     for i in range(nrows):
         # append sentece ID to the correct member list
-        cluster_members[c_labels[i]].append(i)
+        sentence_i_cluster = c_labels[i]
+        cluster_members[sentence_i_cluster].append(i)
+        output.write(sentences[i] + ',' + str(c_labels[i]) + ',' + sentence_files[i] + '\n')
 
     for i in range(1, k+1):
         print("***** cluster: %s, size: %s *****" % (i, len(cluster_members[i])) )
@@ -64,7 +75,23 @@ def main():
             print("cluster %s sentence: " % i, sentences[sent_id])
         input()
 
-
+    # # MDS code from https://baoilleach.blogspot.com/2014/01/convert-distance-matrix-to-2d.html
+    # mds = manifold.MDS(n_components=2, dissimilarity="precomputed", random_state=6)
+    # results = mds.fit(squareform(Y))
+    #
+    # coords = results.embedding_
+    # plt.subplots_adjust(bottom=0.1)
+    # plt.scatter(coords[:, 0], coords[:, 1], marker='o')
+    #
+    # for label, x, y in zip(c_labels, coords[:, 0], coords[:, 1]):
+    #     plt.annotate(
+    #         label,
+    #         xy=(x, y), xytext=(-20, 20),
+    #         textcoords='offset points', ha='right', va='bottom',
+    #         bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
+    #         arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0'))
+    #
+    # plt.show()
 
 
 
---
+++ src/summarize_clusters.py
@@ -0,0 +1,168 @@
+"""
+This is a program to summarize clusters to report over represented words.
+"""
+
+import option_helpers as opth
+import nltk
+import string
+from nltk.corpus import stopwords
+from collections import defaultdict
+
+
+def create_options():
+    # Create some options for the tool.
+    options = opth.default_option_map_input_output()
+    # modify default descriptions for input and output
+    options['input']['description'] = 'A comma separated file of sentences as rows with clusters labeled.'
+    options['input']['input_name'] = '<cluster_file>'
+    options['output']['description'] = 'An output file name for the word distribution data (CSV).'
+    # add custom options
+    options['topN'] = {
+        'order': 3,
+        'short': 'n',
+        'long': 'topN',
+        'input_name': '<number>',
+        'description': 'Return the N words with the highest count for each cluster.',
+        'optional': True
+    }
+    options['cutoff'] = {
+        'order': 4,
+        'short': 'c',
+        'long': 'cutoff',
+        'input_name': '<count_limit>',
+        'description': 'Filter words with less that <count_limit> occurrences. (default: 10)',
+        'optional': True
+    }
+    options['percent'] = {
+        'order': 5,
+        'short': 'p',
+        'long': 'percent',
+        'input_name': '<percent_limit>',
+        'description': 'Filter words that account for less than <percent_limit> of cluster words (default: 0.0,all)',
+        'optional': True
+    }
+    return options
+
+
+def shared_top_10(cluster_word_database):
+    top_10_set = set()
+
+    for cluster in cluster_word_database:
+        word_map = cluster_word_database[cluster]
+        for k, v in sorted(word_map.items(), key=lambda item: item[1], reverse=True)[:min(10, len(word_map))]:
+            top_10_set.add(k)
+
+    return top_10_set
+
+
+def main():
+    options = create_options()
+    program_description = 'Summarize the word distributions of sentence clusters.\n' \
+                          ' Words are selected if their in-cluster frequency\n' \
+                          ' is greater than data set frequency.'
+    print_usage_func = opth.print_usage_maker('Summarize the word distributions of sentence clusters.', options)
+    parse_function = opth.parse_options_maker(options, print_usage_func)
+
+    argument_map = parse_function()
+
+    input_file = opth.validate_required('input', argument_map, print_usage_func)
+    output_file = opth.validate_required('output', argument_map, print_usage_func)
+    count_limit = opth.with_default_int('cutoff', argument_map, 10)
+    percent_limit = opth.with_default_float('percent', argument_map, 0.0)
+
+    top_n = opth.with_default_int('topN', argument_map, None)
+
+    # print(input_file, output_file, count_limit, percent_limit, top_n)
+
+    stops = stopwords.words('english')
+    # print(stops)
+    stops.extend(['america', 'americans'])
+    # print(stops)
+
+    cluster_word_database = {}
+    cluster_word_total = defaultdict(int)
+
+    all_data_word_counts = defaultdict(int)
+    total_words = 0
+
+    with open(input_file, encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+            if line == 'Sentence,cluster,file':
+                # skip header
+                continue
+            # print(line)
+
+            # split sentence record
+            sentence, cluster, filename = line.split(',')
+
+            # initialize cluster database
+            if cluster not in cluster_word_database:
+                cluster_word_database[cluster] = defaultdict(int)
+
+            word_list = nltk.word_tokenize(sentence)
+            for word in word_list:
+                word = word.lower()
+                word = word.strip(string.punctuation)
+                if len(word) == 0 or word in stops:
+                    continue
+
+                cluster_word_database[cluster][word] += 1
+                cluster_word_total[cluster] += 1
+                all_data_word_counts[word] += 1
+                total_words += 1
+
+    # testing stuff that didn't work too well
+    # top_10 = shared_top_10(cluster_word_database)
+    # print(top_10)
+    # print(len(top_10))
+    # input()
+
+    ################
+    # main filtering
+    header = ['cluster', 'word', 'count', 'cluster_freq', 'total_freq']
+    output_rows = []
+
+    for cluster in cluster_word_total:
+        total = float(cluster_word_total[cluster])
+        # print(cluster, total)
+        word_map = cluster_word_database[cluster]
+        # print(word_map)
+
+        # sort all cluster work pairs
+        sorted_pairs = sorted(word_map.items(), key=lambda item: item[1], reverse=True)
+
+        if top_n:
+            limit = min(top_n, len(word_map))
+        else:
+            limit = min(10, len(word_map))
+
+        count = 0
+        for k, v in sorted_pairs:
+            cluster_frequency = v/total
+            dataset_frequency = all_data_word_counts[k]/total_words
+
+            if v < count_limit:
+                continue
+
+            if cluster_frequency < percent_limit:
+                continue
+
+            if cluster_frequency > dataset_frequency and v > 10:
+                # print(k, v, round(v/total, 5))
+                output_rows.append([cluster, k, str(v), str(round(cluster_frequency, 5)),
+                                    str(round(dataset_frequency, 5))])
+                count += 1
+                if count >= limit:
+                    break
+
+    with open(output_file,'w') as f:
+        f.write(','.join(header) + '\n')
+        for row in output_rows:
+            # print(','.join(row))
+            f.write(','.join(row) + '\n')
+
+
+
+
+main()
